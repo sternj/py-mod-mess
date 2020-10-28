cdd78742ad3b177b22a72a53b7b0189a26e7befa
sternj <sternj@brandeis.edu>
2020-10-28 16:48:32 -0400

    removed some more files

--- mess/print_main.py
+++ mess/print_main.py
@@ -25,9 +25,11 @@ def entrypoint():
         import __main__
         the_locals = __main__.__dict__
         the_globals = __main__.__dict__
+        print(the_globals)
         # Splice in the name of the file being executed instead of the profiler.
         print("MAIN ", __main__)
         the_globals["__file__"] = os.path.basename(sys.argv[1])
+        the_globals["__spec__"] = None
         try:
 
             exec(code, the_globals, the_locals)
